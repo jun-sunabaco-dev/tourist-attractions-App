dd40361c475c5e9e5ae815570968ebb50cb32be9
ayaka <ayaka@ayakanoMacBook-Pro.local>
2026-04-17 15:04:56 +0900

    add db config

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+
+# DB設定
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///spots.db'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
+db = SQLAlchemy(app)
+
+# 一覧表示
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# 登録フォーム表示 & 保存
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
